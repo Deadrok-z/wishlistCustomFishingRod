fa8a0fd78211c73a0296a965f225783b1f0fffa6
Deadrok-z <84705521+Deadrok-z@users.noreply.github.com>
2022-07-20 17:20:22 +0300

    fix App class on function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Table from './components/table/table';
 import ButtonTabBar from './components/buttonTabBar/buttonTabBar';
- 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h3 className='d-flex justify-content-left'>Main Page</h3>
-        <ButtonTabBar/>
-        <p className="Table-header">Wide range of available products</p>
-        <Table/>
-      </div>
-    );
-  }
+
+export default function App() {
+  return (
+    <div className="App">
+      <h3 className='d-flex justify-content-left'>Main Page</h3>
+      <ButtonTabBar />
+      <p className="Table-header">Wide range of available products</p>
+      <Table />
+    </div>
+  );
 }
- 
-export default App;
