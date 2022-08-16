4d74da0ffcf94137b236ad5708e236870c591824
Deadrok-z <84705521+Deadrok-z@users.noreply.github.com>
2022-08-16 17:25:42 +0300

    delete dateRods

--- src/features/table/tableSlice.js
+++ src/features/table/tableSlice.js
@@ -1,73 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
-    dateRods: [{
-        "AssemblyType_ID": "P",
-        "Country_ID": "RU",
-        "ID": 0,
-        "Owner": "Fisherman",
-        "Status": "A",
-        "Inserts_ID": "S",
-        "Knee": 3,
-        "MaterialHandle_ID": "E",
-        "MaterialRod_ID": "C",
-        "Weight": 100,
-        "Name": "Volzhanka",
-        "Price": 45,
-        "Length": 3,
-        "ReelSeat_ID": "R",
-        "Rings": 7,
-        "Type_ID": "F",
-        "MinTest": 40,
-        "MaxTest": 250,
-        "LengthKnee": 100,
-        "LengthHandle": 20,
-        "createdBy": "admin"
-    }, {
-        "AssemblyType_ID": "P",
-        "Country_ID": "RU",
-        "ID": 1,
-        "Owner": "Fisherman",
-        "Status": "C",
-        "Inserts_ID": "H",
-        "Knee": 2,
-        "MaterialHandle_ID": "C",
-        "MaterialRod_ID": "F",
-        "Weight": 100,
-        "Name": "Maximus",
-        "Price": 25,
-        "Length": 2,
-        "ReelSeat_ID": "R",
-        "Rings": 7,
-        "Type_ID": "S",
-        "MinTest": 5,
-        "MaxTest": 15,
-        "LengthKnee": 100,
-        "LengthHandle": 20,
-        "createdBy": "admin"
-    }, {
-        "AssemblyType_ID": "T",
-        "Country_ID": "US",
-        "ID": 2,
-        "Owner": "Fisherman",
-        "Status": "A",
-        "Inserts_ID": "G",
-        "Knee": 2,
-        "MaterialHandle_ID": "P",
-        "MaterialRod_ID": "F",
-        "Weight": 500,
-        "Name": "Crocodile",
-        "Price": 15,
-        "Length": 2,
-        "ReelSeat_ID": "S",
-        "Rings": 7,
-        "Type_ID": "R",
-        "MinTest": 50,
-        "MaxTest": 500,
-        "LengthKnee": 100,
-        "LengthHandle": 20,
-        "createdBy": "admin"
-    }]
+    dateRods: []
 }
 
 export const fetchRods = createAsyncThunk('rods/fetchRods', async ()=>{
